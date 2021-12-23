b6fd3c57f1f238e57f690cfa991959a974c7cefa
Piero Toffanin <pt@masseranolabs.com>
2021-12-23 13:23:56 -0500

    Add DLLs directory explicitely to fix Windows imports

--- opensfm/__init__.py
+++ opensfm/__init__.py
@@ -1,3 +1,8 @@
+import os
+import sys
+if sys.platform == 'win32':
+    os.add_dll_directory(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 from opensfm import pybundle
 from opensfm import pydense
 from opensfm import pyfeatures
